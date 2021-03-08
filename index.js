4acd7f21b5103f6657f8b6c46585c158f78a4eac
iagocardial <iagocardial@tecsus.com.br>
2021-03-08 19:50:26 -0300

    Adicionando ao express a engine EJS

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const app = express();
 
+//Falando ao express que a view engine é o EJS
+app.set('view engine', 'ejs');
+
 app.get("/", () => {
     res.send("Bem vindo ao meu site");
 });
 
 app.listen(8080, ()=> {
-
+    console.log("App Rodando!");
 });
