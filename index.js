083ec59b951e76255cfbeb7d8e12bf875c8fb6c2
iagocardial <iagocardial@tecsus.com.br>
2021-03-08 20:39:13 -0300

    Adicionando Bootstrap ao projeto

--- index.js
+++ index.js
@@ -3,29 +3,10 @@ const app = express();
 
 //Falando ao express que a view engine é o EJS
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/:nome/:lang", (req, res) => {
-    let nome = req.params.nome;
-    let lang = req.params.lang;
-    let exibirMsg = true;
-
-    let produtos = [
-        {nome: "Doritos", preco: 3.14},
-        {nome: "Coca-Cola", preco: 5},
-        {nome: "Leite", preco: 1.15},
-        {nome: "Carne", preco: 15},
-        {nome: "Red-Bull", preco: 6},
-        {nome: "Nescau", preco: 6},
-    ];
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Empresa",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+    res.render("index")
 });
 
 app.listen(8080, ()=> {
