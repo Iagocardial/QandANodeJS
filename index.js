d518035b3507a7354118aa1366fc94a520914f72
iagocardial <iagocardial@tecsus.com.br>
2021-03-11 20:32:51 -0300

    Adicionando modulo Sequelize para usar com MYSQL

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 //Falando ao express que a view engine é o EJS
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
+//para configurar o bodyparser para recebimento de dados
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json())
 
 app.get("/", (req, res) => {
     res.render("index")
@@ -18,5 +23,7 @@ app.listen(8080, ()=> {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulário recebido")
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulário recebido! titulo = "+ titulo + " e a descricao = " + descricao);
 });
